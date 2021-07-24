5c4abd8a43d5b38d1cdaf5225e3c53394bfc45b1
Jaron <jaron_zijlstra@outlook.com>
2021-07-24 22:18:41 +0200

    Feat: added cancelIgnore option

--- src/main/java/io/github/jaronz/mwcommandblacklist/EventListener.java
+++ src/main/java/io/github/jaronz/mwcommandblacklist/EventListener.java
@@ -13,13 +13,11 @@ public class EventListener implements Listener {
     @EventHandler
     public void onCommand(PlayerCommandPreprocessEvent event){
         Player player = event.getPlayer();
-
-        if(player.hasPermission("mwcommandblacklist.ignore")) return;
-
         Config config = MWCommandBlacklist.instance.getConfig();
-        List<Map> blockedCommands = config.getMapList("commands");
 
-        for(Map blockedCommand : blockedCommands){
+        for(Map blockedCommand : config.getMapList("commands")){
+            if(player.hasPermission("mwcommandblacklist.ignore") &&
+                !(boolean)blockedCommand.getOrDefault("cancelIgnore", false)) continue;
             String command = blockedCommand.get("command").toString().toLowerCase();
             if(!command.startsWith("/")) command = "/" + command;
             if(event.getMessage().toLowerCase().startsWith(command)){
